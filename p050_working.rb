0fee165c3521c556089d2d42c2fac1db4a13172d
Joseph Mastey <jmmastey@gmail.com>
2010-12-20 15:52:29 -0600

    why is trial division so fast?

--- p050_working.rb
+++ p050_working.rb
@@ -2,6 +2,10 @@ require 'tools'
 
 # finds the right answer in about 5 seconds, but exhaustively searches
 # the rest of the space for another minute or so before coming to a stop.
+#
+# this should actually be sped up much further by observing the maximum
+# chain length yeiling N < 1M (which is 546). another minor optimization
+# would be to use a smarter way to get the sum
 
 lim = 1_000_000
 @primes = Sieve.get_primes_to(lim/2)
---
+++ p058.rb
@@ -0,0 +1,27 @@
+require 'tools'
+
+@lookup = Sieve.quick_primes_to_1m
+primes = 3
+total = 5
+
+sl = 3
+ctr = 9
+
+while true
+  sl += 2
+  mv = sl - 1
+
+  primes += 1 if (ctr+mv).prime_by_trial?(@lookup)
+  primes += 1 if (ctr+2*mv).prime_by_trial?(@lookup)
+  primes += 1 if (ctr+3*mv).prime_by_trial?(@lookup)
+
+  total += 4
+  ctr += 4*mv
+
+  # debug
+  #puts "#{ctr} #{primes} #{total} #{sl}"
+  next unless primes*10 < total
+
+  p sl
+  exit
+end

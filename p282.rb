9fdb01057ab85fa9d869309db1e835880dcb6dbb
Joseph Mastey <jmmastey@gmail.com>
2011-01-03 20:16:05 -0600

    First blush at big ackermans. Obviously useless at this rate.

---
+++ p282.rb
@@ -0,0 +1,15 @@
+# need to calculate this over a modulo...
+
+@ack = { "1:1" => 3, "2:2" => 7, "3:3" => 61, }
+def ack(m, n)
+  key = "#{m}:#{n}"
+  puts "#{key} #{@ack.length}"
+  return @ack[key] if @ack.include? key
+  return (@ack[key] = n + 1) if m == 0
+  return (@ack[key] = ack(m-1,1)) if n == 0
+  return (@ack[key] = ack(m-1, ack(m, n-1)))
+end
+
+1.upto 5 do |n|
+  puts ">> #{n}:#{n}: #{ack(n,n)}"
+end
